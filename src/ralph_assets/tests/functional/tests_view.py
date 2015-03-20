d1bea5a1ed64f8c41983b94995309a37f9bf0802
Arkadiusz Adamski <arkadiusz.adamski@ext.allegrogroup.com>
2015-03-20 10:58:34 +0100

    WIP

--- src/ralph_assets/tests/functional/tests_view.py
+++ src/ralph_assets/tests/functional/tests_view.py
@@ -192,21 +192,7 @@ class TestRegions(object):
         self.assertEqual(response.status_code, 404)
 
 
-class Singleton(object):
-
-    __instance__ = None
-
-    def __new__(cls, *a, **kw):
-        if Singleton.__instance__ is None:
-            Singleton.__instance__ = object.__new__(cls, *a, **kw)
-            cls._Singleton__instance = Singleton.__instance__
-        return Singleton.__instance__
-
-    def _drop_it(self):
-        Singleton.__instance__ = None
-
-
-class BaseViewsTest(ClientMixin, TransactionTestCase, Singleton):
+class BaseViewsTest(ClientMixin, TransactionTestCase, ):
     client_class = AjaxClient
     password = 'ralph'
 
